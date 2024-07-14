e3a58313e09ccde8d5c0244715f276ae63325089
Andrei Bachinin <limbus727@gmail.com>
2024-07-14 13:53:43 -0600

    AppState and Events are updated

--- src/components/base/ApplicationEvents.ts
+++ src/components/base/ApplicationEvents.ts
@@ -1,11 +1,15 @@
 export enum ApplicationEvents {
 	// Не Конечный Список
+	AddressErrorsChange = 'addressErrors:change',
 	BasketChange = 'basket:change',
 	BasketOpen = 'basket:open',
 	BasketAdd = 'basket:add',
 	BasketRemove = 'basket:remove',
+	ContactsErrorsChange = 'contactsErrors:change',
+	ItemsChange = 'items:change',
 	OrderReady = 'order:ready',
 	OrderOpen = 'order:open',
 	OrderSubmit = 'order:submit',
 	OrderSuccess = 'order:success',
+	PreviewChange = 'preview:change',
 }
--- src/components/model/AppState.ts
+++ src/components/model/AppState.ts
@@ -7,10 +7,12 @@ import {
 	IOrderAddress,
 	IOrderContacts,
 } from '../../types';
+import { ApplicationEvents } from '../base/ApplicationEvents';
 
-// =======================
-// Реализация Не Закончена
-// =======================
+export type CatalogChangeEvent = {
+	catalog: IProduct[];
+};
+
 export class AppState extends Model<IAppState> {
 	basket: IProduct[] = [];
 	catalog: IProduct[];
@@ -29,51 +31,83 @@ export class AppState extends Model<IAppState> {
 		return this.basket;
 	}
 
-	addToBasket(product: IProduct): void {
-		return;
+	addToBasket(product: IProduct) {
+		this.basket.push(product);
+		this.emitChanges(ApplicationEvents.BasketChange);
 	}
 
-	removeFromBasket(product: IProduct): void {
-		return;
+	removeFromBasket(product: IProduct) {
+		this.basket = this.basket.filter((item) => item.id !== product.id);
+		this.emitChanges(ApplicationEvents.BasketChange);
 	}
 
-	clearBasket(): void {
-		return;
+	clearBasket() {
+		this.basket = [];
+		this.emitChanges(ApplicationEvents.BasketChange);
 	}
 
 	getTotal(): number {
-		return;
+		return this.basket.reduce((sum, item) => sum + item.price, 0);
 	}
 
-	setCatalog(products: IProduct[]): void {
-		return;
+	setCatalog(products: IProduct[]) {
+		this.catalog = products;
+		this.emitChanges(ApplicationEvents.ItemsChange, {
+			catalog: this.catalog,
+		});
 	}
 
-	setPreview(product: IProduct): void {
-		return;
+	setPreview(product: IProduct) {
+		this.preview = product.id;
+		this.emitChanges(ApplicationEvents.PreviewChange, product);
 	}
 
 	isProductInBasket(product: IProduct): boolean {
-		return;
+		return this.basket.some((basketItem) => basketItem.id === product.id);
 	}
 
-	setOrder(): void {
-		return;
-	}
-
-	setOrderField(field: keyof IOrderAddress, value: string): void {
-		return;
-	}
-
-	setContactsField(field: keyof IOrderContacts, value: string): void {
-		return;
+	setOrder() {
+		this.order.items = this.getBasketItems().map((product) => product.id);
+		this.order.total = this.getTotal();
 	}
 
 	validateOrderPaymentMethod() {
-		return;
+		const errors: typeof this.formErrors = {};
+		if (!this.order.payment) {
+			errors.payment = 'Выберите способ оплаты';
+		}
+		if (!this.order.address) {
+			errors.address = 'Введите адрес доставки';
+		}
+		this.formErrors = errors;
+		this.events.emit(ApplicationEvents.AddressErrorsChange, this.formErrors);
+		return Object.keys(errors).length === 0;
 	}
 
 	validateOrderContacts() {
-		return;
+		const errors: typeof this.formErrors = {};
+		if (!this.order.email) {
+			errors.email = 'Введите Email';
+		}
+		if (!this.order.phone) {
+			errors.phone = 'Введите номер телефона';
+		}
+		this.formErrors = errors;
+		this.events.emit(ApplicationEvents.ContactsErrorsChange, this.formErrors);
+		return Object.keys(errors).length === 0;
+	}
+
+	setOrderField(field: keyof IOrderAddress, value: string) {
+		this.order[field] = value;
+		if (this.validateOrderPaymentMethod()) {
+			this.events.emit(ApplicationEvents.OrderReady, this.order);
+		}
+	}
+
+	setContactsField(field: keyof IOrderContacts, value: string) {
+		this.order[field] = value;
+		if (this.validateOrderContacts()) {
+			this.events.emit(ApplicationEvents.OrderReady, this.order);
+		}
 	}
 }
